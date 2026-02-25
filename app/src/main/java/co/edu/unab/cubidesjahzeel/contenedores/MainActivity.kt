dba5b00e9e90306baa12071b730e2b1a3ec776ca
Laboratorio de Telecomunicaciones <cisco@academico.unab.ad>
2026-02-25 09:19:00 -0500

    Contendores

--- app/src/main/java/co/edu/unab/cubidesjahzeel/contenedores/MainActivity.kt
+++ app/src/main/java/co/edu/unab/cubidesjahzeel/contenedores/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
@@ -12,6 +13,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -20,6 +24,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.graphics.Color.Companion.Yellow
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -88,4 +95,40 @@ fun ContentScreen(){
         }
     }
 }
+
+@Preview(showSystemUi = true)
+@Composable
+fun ContentScreen(){
+    Column(
+        modifier = Modifier
+            .background(Color.White)
+            .padding(18.dp)
+            .fillMaxSize(),
+
+        horizontalAlignment = Alignment.CenterHorizontally){
+
+        Row(
+            modifier = Modifier
+                .background(Color.White)
+                .padding(all = 16.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+
+        }
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.aprovado),
+                contentDescription = "Hola mundo",
+                modifier = Modifier.size(200.dp)
+            )
+        }
+
+    }
+}
+
 
